ce48f4e8feb0340d9380cfd7d03d2150e31a7ce4
kudaken0 <kudaken.sub@gmail.com>
2026-09-25 22:51:10 +0900

    ジャンク大好き人間とナオノットの位置を入れ替え close #5

--- src/data/members.ts
+++ src/data/members.ts
@@ -99,17 +99,6 @@ export const members: Member[] = [
     ],
   },
   {
-    slug: 'naonot',
-    name: 'ナオノット',
-    nameEn: 'NAONOT',
-    role: '運搬 / 搬入 / サポート',
-    icon: '/assets/members/naonot.jpeg',
-    bio: [
-      'オーエンを愛し続ける狂人ナオノットです!!\n小学2年の時からオーエン中毒者やってます!!\n神奈川の民基本搬入、運搬をメインで基本出来る事はなんでもやる奴',
-    ],
-    socials: [{ label: 'X / TWITTER', href: 'https://x.com/nao_not1222' }],
-  },
-  {
     slug: 'junkdaisuki',
     name: 'ジャンク大好き人間',
     nameEn: 'junkdaisuki',
@@ -127,4 +116,16 @@ export const members: Member[] = [
       { label: 'MISSKEY', href: 'https://misskey.io/@DSidaisuki' },
     ],
   },
+  {
+    slug: 'naonot',
+    name: 'ナオノット',
+    nameEn: 'NAONOT',
+    role: '運搬 / 搬入 / サポート',
+    icon: '/assets/members/naonot.jpeg',
+    bio: [
+      'オーエンを愛し続ける狂人ナオノットです!!\n小学2年の時からオーエン中毒者やってます!!\n神奈川の民基本搬入、運搬をメインで基本出来る事はなんでもやる奴',
+    ],
+    socials: [{ label: 'X / TWITTER', href: 'https://x.com/nao_not1222' }],
+  },
+
 ];
